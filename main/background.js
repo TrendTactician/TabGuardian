6ec2e34b167623dfe33fd77e7348375cef29fa6a
Saurabh <87906226+SpQpS@users.noreply.github.com>
2023-12-07 00:03:32 +0530

    Update background.js

--- main/background.js
+++ main/background.js
@@ -1,7 +1,48 @@
 // background.js
+let closedTabsCount = 0;
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({
+    enableNormal: true,
+    enableIncognito: true,
+    closedTabsCount: 0
+  });
+});
+
 chrome.tabs.onCreated.addListener(function(tab) {
-  // Close the tab if it was opened by a website
-  if (tab.openerTabId !== undefined) {
-    chrome.tabs.remove(tab.id);
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const activeTab = tabs[0];
+    const isIncognito = activeTab.incognito;
+
+    chrome.storage.sync.get(['enableNormal', 'enableIncognito', 'closedTabsCount'], function(result) {
+      const shouldCloseTab = tab.openerTabId !== undefined &&
+        ((isIncognito && result.enableIncognito) || (!isIncognito && result.enableNormal));
+
+      if (shouldCloseTab) {
+        chrome.tabs.remove(tab.id);
+        closedTabsCount++;
+        updateBadgeText(closedTabsCount);
+        chrome.storage.sync.set({ closedTabsCount: closedTabsCount });
+      }
+    });
+  });
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === 'toggleExtension') {
+    chrome.storage.sync.get(['enableNormal', 'enableIncognito'], function(result) {
+      const enableNormal = !result.enableNormal;
+      const enableIncognito = !result.enableIncognito;
+
+      chrome.storage.sync.set({
+        enableNormal: enableNormal,
+        enableIncognito: enableIncognito
+      });
+    });
   }
 });
+
+function updateBadgeText(count) {
+  const badgeText = count > 0 ? count.toString() : '';
+  chrome.browserAction.setBadgeText({ text: badgeText });
+}
